72b88773c71e71dc809a9e67b10edf8101874952
hhh5666 <69581221+hhh5666@users.noreply.github.com>
2025-07-13 14:17:06 +0300

    Update app.py __

--- backend/app.py
+++ backend/app.py
@@ -85,6 +85,6 @@ def add_review():
     save_data("data/reviews.json", data)
     return jsonify({"success": True})
 
-if name == "main":
+if __name__ == "__main__":
     from waitress import serve
     serve(app, host="0.0.0.0", port=8080)
